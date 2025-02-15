f1c601a93df1a78210c5cfaaf595dd58a0551d04
Exp <17885980+exp111@users.noreply.github.com>
2025-02-15 15:57:29 +0100

    package

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-    })
+        width: 1280,
+        height: 720,
+        autoHideMenuBar: true
+    });
 
     win.loadFile('docs/index.html')
 }
